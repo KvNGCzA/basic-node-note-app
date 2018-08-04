b6948b876c0e2a69779cb17e76c80397b28c97a3
KvNGCzA <akanmuoluwadamilare@yahoo.com>
2018-08-04 15:46:22 +0100

    4 8 2018

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ console.log('Yargs: ',argv);
 switch (cmd) {
     case 'add': 
     let note = notes.addNote(argv.title, argv.body);
-    console.log('Note check:', note);
        if( note ){
         console.log(`Note: ${note.title} saved successfully with Body: ${note.body}`);
        }
@@ -29,24 +28,49 @@ switch (cmd) {
     break;
     case 'read':
         let readNote = notes.getNote(argv.title);
-        console.log(`${argv.title}: ${readNote}`);
+        if(!readNote){
+            console.log('This note does not exist!');
+        }
+        else{
+            console.log(`${argv.title}: ${readNote}`);
+        }        
     break;
     case 'remove':
         let rNote = notes.remNote(argv.title);
         console.log(`Removing note ${argv.title}`);
-        console.log(`Note ${argv.title} removed`);
-        console.log(rNote);
+        if(!rNote){
+            console.log('This not does not exist!');
+        }
+        else{            
+            console.log(`Note ${argv.title} removed`);
+            console.log(rNote);
+        }
     break;
     case 'update-body':
-        let update = notes.updateBody(argv.title, argv.body);
-        if(!update){
-            console.log("Nothing to update");
+        let updateB = notes.updateBody(argv.title, argv.body);
+        if(!updateB){
+            console.log("Nothing to update!");
+        }
+        else if(updateB === "Does not exist"){
+            console.log("This title does not exist");
         }
         else{
-            console.log(`${update.title}'s body has been updated to: '${update.body}'`);
+            console.log(`${updateB.title}'s body has been updated to: '${updateB.body}'`);
+        }
+    break;
+    case 'update-title':
+        let updateT = notes.updateTitle(argv.title, argv.newTitle);
+        if(!updateT){
+            console.log('This note does not exist!');
+        }
+        else if(typeof updateT === 'string'){
+            console.log("New title is same as old title");
+        }
+        else{
+            console.log(`Note ${argv.title}'s title has been updated to "${argv.newTitle}"`);
         }
     break;
     default:
-        console.log('Command not recognized');
+        console.log('Command not recognized!');
     break;
 }
--- notes.js
+++ notes.js
@@ -13,17 +13,25 @@ const fetch = () => {
 const save = (x) => {
     fs.writeFileSync('notes-data.json',JSON.stringify(x));
 }
+const duplicateNote = (title) =>{    
+    let notes = fetch();
+    return notes.filter( note => note.title === title );
+}
+const nonDuplicateNote = (title) =>{
+    let notes = fetch();
+    return notes.filter( note => note.title !== title );
+}
+
 
 addNote = (title, body) => {
-    let notes = fetch();
     let note = {
         title,
         body
     };
-    let dupNotes = notes.filter( note => note.title === title );
+    let dupNotes = duplicateNote(title);
 
     if (dupNotes.length === 0) {
-        notes.push(note);
+        notes.unshift(note);
         save(notes);
         console.log(notes);
         return note;
@@ -35,38 +43,60 @@ getAll = () => {
 };
 
 getNote = title => {
-    let notes = fetch();
-    let readNote = notes.filter( note => note.title === title);
-    return readNote[0].body;
+    let dupNote = duplicateNote(title);
+    if( dupNote.length === 0){
+        return false;
+    }
+    return dupNote[0].body;
 };
 
 remNote = title => {
-    let notes = fetch();
-    let rNote = notes.filter(note => note.title !== title);
-    save(rNote);
-    return rNote;
+    let nonDupNote = nonDuplicateNote(title);
+    if( nonDupNote.length === notes.length){
+        return false;
+    }
+    save(nonDupNote);
+    return nonDupNote;
     
 };
 
 updateBody = (title, body) => {
-    let notes = fetch();
-    let newNote = notes.filter(note => note.title !== title);
-    let upNote = notes.filter(note => note.title === title);
-    if(upNote[0].body === body){
+    let nonDupNote = nonDuplicateNote(title);
+    let dupNote = duplicateNote(title);
+    if( dupNote.length === 0){
+        return "Does not exist";
+    }
+    else if(dupNote[0].body === body){
         return false;
     }
-    upNote[0].body = body;
-    let final = [...upNote, ...newNote];
+    dupNote[0].body = body;
+    let final = [...dupNote, ...nonDupNote];
     save(final);
     console.log(final);
-    return upNote[0];
-}
+    return dupNote[0];
+};
+
+updateTitle = (title, newTitle) => {
+    let nonDupNote = nonDuplicateNote(title);   
+    let dupNote = duplicateNote(title);
+    if(title === newTitle){
+        return "New title is same as old title";
+    }
+    else if(dupNote.length === 0){
+        return false;
+    }
+    dupNote[0].title = newTitle;
+    let final = [...dupNote, ...nonDupNote]
+    save(final);
+    return dupNote;
+};
 
 module.exports = {
     addNote,
     getAll,
     getNote,
     remNote,
-    updateBody
+    updateBody,
+    updateTitle
 };
 
